ac4e553f208d6b0b163f7bdf05066dcc36a4932b
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-07-10 20:26:56 +0200

    Create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,7 @@
+const = "No argument";
+const = "Argument found"
+const = "Arguments found"
+
+console.log("No argument");
+console.log("Argument found”);
+console.log("Arguments found”);            
