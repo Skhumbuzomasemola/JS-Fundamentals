ab2366e14ecf61489016151c7f2cf5d78df92bef
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-07-10 20:21:03 +0200

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,7 @@
-C is fun
-Python is cool
-JavaScript is amazing
-console.log('C is fun '' Python is cool '' JavaScript is amazing);
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
